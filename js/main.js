e8a6296639d477ba09e4f4aab468bc2b567d7390
Gyskard <gyskard@protonmail.com>
2018-04-07 21:39:34 +0200

    prototype

--- js/main.js
+++ js/main.js
@@ -7,15 +7,15 @@ $('#but1').click(function () {
         $('#but2').removeClass('btn-primary').addClass('btn-secondary');
         if (toggle === false)
         {
-            $('#menu').animate({marginTop:'10em'},500,'swing');
-            $('<hr>').appendTo($('section')).css("width", "20em").addClass('animated zoomIn');
             toggle = true;
+            $('#menu').animate({marginTop:'10em'},500,'swing');
+            $('<hr>').appendTo($('section')).css("width", "30em").addClass('animated zoomIn mt-4');
             $.ajax({
                 url : './data/generator.html',
                 type : 'GET',
                 dataType : 'html',
                 success : function(html) {
-                    console.log(html)
+                    html.appendTo($(body));
                 }
             });
         }
